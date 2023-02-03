f03cd083baaa587a421048aa15a72acdc8d7aaad
Harold Go <104198280+haroldgo23@users.noreply.github.com>
2023-02-03 15:03:04 +0800

    Preprocessing dataset

---
+++ extension/preprocessing.py
@@ -0,0 +1,17 @@
+import re
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import string
+
+# Remove extra characters
+def wordopt(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub("\\W"," ",text) 
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)    
+    return text
